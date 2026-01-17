b73f28b1375844966fe6799aab4cadf85de7f8be
gw31415 <24710985+gw31415@users.noreply.github.com>
2026-01-17 18:04:12 +0900

    feat: Add UTF-8 charset meta tag

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -5,6 +5,8 @@ export const renderer = reactRenderer(({ children }) => {
   return (
     <html lang="ja">
       <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <ViteClient />
         <Link href="/src/style.css" rel="stylesheet" />
         <Script src="/src/client.tsx" defer />
