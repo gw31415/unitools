786d9f82d0fa1e93f14203378496d74a71fbf9b5
gw31415 <24710985+gw31415@users.noreply.github.com>
2026-01-20 07:49:06 +0900

    fix: disable undo/redo of starterkit because collaboration has own one

--- src/lib/editorExtensions.ts
+++ src/lib/editorExtensions.ts
@@ -1,4 +1,7 @@
 import { Markdown as MarkdownExt } from "@tiptap/markdown";
 import StarterKit from "@tiptap/starter-kit";
 
-export const baseExtensions = [StarterKit, MarkdownExt];
+export const baseExtensions = [
+  StarterKit.configure({ undoRedo: false }),
+  MarkdownExt,
+];
