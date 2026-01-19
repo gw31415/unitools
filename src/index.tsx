5e4e4eccd741922f8772fe2be859dbad185fd909
gw31415 <24710985+gw31415@users.noreply.github.com>
2026-01-19 15:01:30 +0900

    feat: Add aria-labels for accessibility, robots.txt

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import { App, renderer } from "@/app";
 
 const app = new Hono()
   .use(renderer)
+  // Currently development only disallows all robots
+  .get("/robots.txt", (c) => c.text("User-agent: *\nDisallow: /\n"))
   .get("*", (c) => c.render(<App path={c.req.path} />));
 
 export default app;
