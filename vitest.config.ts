85390ea0ba3f085cccb6b4714910ab55f9aaf827
gw31415 <24710985+gw31415@users.noreply.github.com>
2026-03-13 16:39:36 +0900

    feat: Uncomment tsconfigPaths in config

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  // resolve: { tsconfigPaths: true },
+  resolve: { tsconfigPaths: true },
   plugins: [react()],
   test: {
     environment: "jsdom",
