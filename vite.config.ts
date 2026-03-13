6df054b788f159075515c78036d7439a691d3924
gw31415 <24710985+gw31415@users.noreply.github.com>
2026-03-13 13:45:55 +0900

    refactor: rm vite-tsconfig-paths because it is not needed anymore

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import ssrPlugin from "vite-ssr-components/plugin";
-import tsconfig from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tsconfig(), cloudflare(), tailwindcss(), ssrPlugin()],
+  resolve: { tsconfigPaths: true },
+  plugins: [cloudflare(), tailwindcss(), ssrPlugin()],
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,9 @@
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  // resolve: { tsconfigPaths: true },
+  plugins: [react()],
   test: {
     environment: "jsdom",
     globals: true,
